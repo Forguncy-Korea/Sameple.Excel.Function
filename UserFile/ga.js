e9496d9bf7f5a1ce051e6fde319f15d1719b7867
Forguncy-Korea <jake.park@grapecity.com>
2022-05-11 15:37:05 +0900

    {"Message":"원격 프로젝트를 초기화합니다.","Modules":[{"State":3,"Type":6,"Name":"Pages/기본 제공 페이지"},{"State":3,"Type":1,"Name":"PCPage/FGC_401_권한없음"},{"State":3,"Type":1,"Name":"PCPage/FGC_로그인"},{"State":3,"Type":1,"Name":"PCPage/FGC_비밀번호분실"},{"State":3,"Type":1,"Name":"PCPage/FGC_임시점검중"},{"State":3,"Type":1,"Name":"PCPage/FGC_비밀번호변경"},{"State":3,"Type":1,"Name":"PCPage/FGC_사용자정보편집"},{"State":3,"Type":1,"Name":"PCPage/FGC_이메일알림설정"},{"State":3,"Type":1,"Name":"PCPage/FGC_비밀번호재설정"},{"State":3,"Type":1,"Name":"PCPage/FGC_비밀번호재설정링크보내기"},{"State":3,"Type":1,"Name":"PCPage/FGC_2단계인증_인증코드"},{"State":3,"Type":1,"Name":"PCPage/FGC_2단계인증_코드확인"},{"State":3,"Type":1,"Name":"PCPage/teapot"},{"State":3,"Type":6,"Name":"Pages/기본 제공 모바일 페이지"},{"State":3,"Type":2,"Name":"MobilePage/FGC_401_권한없음_모바일"},{"State":3,"Type":2,"Name":"MobilePage/FGC_비밀번호분실_모바일"},{"State":3,"Type":2,"Name":"MobilePage/FGC_임시점검중_모바일"},{"State":3,"Type":2,"Name":"MobilePage/FGC_비밀번호변경_모바일"},{"State":3,"Type":2,"Name":"MobilePage/FGC_사용자정보편집_모바일"},{"State":3,"Type":2,"Name":"MobilePage/FGC_이메일알림설정_모바일"},{"State":3,"Type":2,"Name":"MobilePage/FGC_로그인_모바일"},{"State":3,"Type":2,"Name":"MobilePage/FGC_비밀번호재설정_모바일"},{"State":3,"Type":2,"Name":"MobilePage/FGC_비밀번호재설정링크보내기_모바일"},{"State":3,"Type":2,"Name":"MobilePage/FGC_2단계인증_인증코드_모바일"},{"State":3,"Type":2,"Name":"MobilePage/FGC_2단계인증_코드확인_모바일"},{"State":3,"Type":1,"Name":"PCPage/FAQ-Features-ImplementExcel"},{"State":3,"Type":6,"Name":"Pages/Excel Implementation"},{"State":3,"Type":1,"Name":"PCPage/A"},{"State":3,"Type":1,"Name":"PCPage/B"},{"State":3,"Type":1,"Name":"PCPage/C"},{"State":3,"Type":1,"Name":"PCPage/D"},{"State":3,"Type":1,"Name":"PCPage/E"},{"State":3,"Type":1,"Name":"PCPage/F"},{"State":3,"Type":1,"Name":"PCPage/G"},{"State":3,"Type":1,"Name":"PCPage/H"},{"State":3,"Type":1,"Name":"PCPage/I"},{"State":3,"Type":1,"Name":"PCPage/J"},{"State":3,"Type":1,"Name":"PCPage/K"},{"State":3,"Type":1,"Name":"PCPage/L"},{"State":3,"Type":1,"Name":"PCPage/M"},{"State":3,"Type":1,"Name":"PCPage/N"},{"State":3,"Type":1,"Name":"PCPage/O"},{"State":3,"Type":1,"Name":"PCPage/P"},{"State":3,"Type":1,"Name":"PCPage/Q"},{"State":3,"Type":1,"Name":"PCPage/R"},{"State":3,"Type":1,"Name":"PCPage/S"},{"State":3,"Type":1,"Name":"PCPage/T"},{"State":3,"Type":1,"Name":"PCPage/U"},{"State":3,"Type":1,"Name":"PCPage/V"},{"State":3,"Type":1,"Name":"PCPage/W"},{"State":3,"Type":1,"Name":"PCPage/X"},{"State":3,"Type":1,"Name":"PCPage/Y"},{"State":3,"Type":1,"Name":"PCPage/Z"},{"State":3,"Type":3,"Name":"MasterPCPage/FGC_유저마스터페이지"},{"State":3,"Type":8,"Name":"ApplicationSetting"},{"State":3,"Type":9,"Name":"Theme"},{"State":3,"Type":10,"Name":"ImageSelector"},{"State":3,"Type":11,"Name":"UserService"},{"State":3,"Type":12,"Name":"CustomLibrary"},{"State":3,"Type":13,"Name":"ShareCommand"},{"State":3,"Type":14,"Name":"MapJsonFile"},{"State":3,"Type":25,"Name":"DataContectionSet"},{"State":3,"Type":26,"Name":"Upload"}],"Version":"v1"}

---
+++ UserFile/ga.js
@@ -0,0 +1,2 @@
+$(document.body).append('<script async src="https://www.googletagmanager.com/gtag/js?id=G-NS0H8CYL2G"></script>');
+$(document.body).append("<script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-NS0H8CYL2G'); </script>");
